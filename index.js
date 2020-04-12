90a54bcaee109ed074f39dc5aed4203ad252eef1
max bittker <maxbittker@gmail.com>
2020-04-12 13:47:11 -0700

    idK

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ var player = require("play-sound")((opts = { player: "aplay" }));
 const path = require("path");
 const fs = require("fs");
 
+function hash(n) {
+  return Math.floor((Math.sin(n) + 1.0) * 43758.5453123);
+}
+
 let sounds = [];
 fs.readdir(path.join(__dirname, "sounds"), function(err, files) {
   if (err) throw err;
@@ -12,17 +16,13 @@ fs.readdir(path.join(__dirname, "sounds"), function(err, files) {
   console.log(files);
 });
 
-function cleanUp() {
-  audio.kill();
-  i3wm.Client.disconnect(client);
-}
-let i = 5;
-function click() {
+function click(x) {
+  let i = hash(x) % sounds.length;
   player.play(
     path.join(__dirname, "sounds", sounds[i]),
     { timeout: 300 },
     function(err) {
-      i = (i + 0) % sounds.length;
+      console.log(sounds[i]);
       if (err) throw err;
     }
   );
@@ -33,14 +33,35 @@ i3wm.Client.connect().then(client => {
   client.subscribe("window", "workspace");
 
   client.on("workspace", msg => {
-    console.log(msg.change);
-    click();
+    let { width, height } = msg.current.rect;
+    click(width * height * 3);
   });
 
   client.on("window", msg => {
-    console.log(msg.change);
+    let { width, height } = msg.container.rect;
     if (["focus", "move", "fullscreen_mode"].indexOf(msg.change) > -1) {
-      click();
+      click(width * height);
     }
   });
 });
+
+function exitHandler(options, exitCode) {
+  i3wm.Client.disconnect(client);
+
+  if (options.cleanup) console.log("clean");
+  if (exitCode || exitCode === 0) console.log(exitCode);
+  if (options.exit) process.exit();
+}
+
+//do something when app is closing
+process.on("exit", exitHandler.bind(null, { cleanup: true }));
+
+//catches ctrl+c event
+process.on("SIGINT", exitHandler.bind(null, { exit: true }));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on("SIGUSR1", exitHandler.bind(null, { exit: true }));
+process.on("SIGUSR2", exitHandler.bind(null, { exit: true }));
+
+//catches uncaught exceptions
+process.on("uncaughtException", exitHandler.bind(null, { exit: true }));
